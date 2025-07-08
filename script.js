9833af6b9cd04ab38ea3572d55d755fda6eb10cf
Aryanarya01 <aryanarya2777@gmial.com>
2025-07-08 22:14:00 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// let n=5;
+// for(i=0;i<n;i++)
+// {
+//     console.log("hello",i); 
+// }
+
+
+
+
+// // console.log(process.argv);
+
+// let args = process.argv;
+// for(i=0;i<args.length;i++)
+// {
+//     console.log("hello to ",args[i]);
+// }
+
+// const math = require("./math");
+// console.log(math.sum(2,2));
+// console.log(math.mul(2,2));
+// console.log(math.PI);
+
+const info = require("./fruits");
+console.log(info);
+
+
+
+
+// import{sum,PI} from "./math.js";
+// import {generate} from "random-words"
+// // console.log(sum(5,20));
+
+// console.log(generate());
